9e537790b6833cb674a65eaebb47826d1e99de1e
DextrousDavid <aimakhededavido@gmail.com>
2020-04-11 07:49:05 +0100

    info things

--- src/estimator.js
+++ src/estimator.js
@@ -1,5 +1,5 @@
-const covid19ImpactEstimator = () => {
-  const data = {
+const covid19ImpactEstimator = (data) => {
+  const info = {
     region: {
       name: 'Africa',
       avgAge: 19.7,
@@ -13,37 +13,37 @@ const covid19ImpactEstimator = () => {
     totalHospitalBeds: 1380614
   };
   const dataCalc = () => {
-    if (data.periodType === 'days') {
-      return (Math.trunc((data.timeToElapse / 3)));
+    if (info.periodType === 'days') {
+      return (Math.trunc((info.timeToElapse / 3)));
     }
-    if (data.periodType === 'weeks') {
-      return (Math.trunc((data.timeToElapse / 3) * 7));
+    if (info.periodType === 'weeks') {
+      return (Math.trunc((info.timeToElapse / 3) * 7));
     }
-    if (data.periodType === 'months') {
-      return (Math.trunc((data.timeToElapse / 3) * 30));
+    if (info.periodType === 'months') {
+      return (Math.trunc((info.timeToElapse / 3) * 30));
     }
     return 0;
   };
   //  Impact
   const impact = {
-    currentlyInfected: data.reportedCases * 10,
-    infectionByRequestedTime: (data.reportedCases * 10) * (2 ** (dataCalc()))
+    currentlyInfected: info.reportedCases * 10,
+    infectionByRequestedTime: (info.reportedCases * 10) * (2 ** (dataCalc()))
   };
   //  SevereImpact
   const severeImpact = {
-    currentlyInfected: data.reportedCases * 50,
-    infectionByRequestedTime: (data.reportedCases * 50) * (2 ** (dataCalc()))
+    currentlyInfected: info.reportedCases * 50,
+    infectionByRequestedTime: (info.reportedCases * 50) * (2 ** (dataCalc()))
   };
-  const input = data;
   const impactEstimator = impact;
   const severeImpactEstimator = severeImpact;
   return {
-    data: input,
+    info,
+    data,
     impact: impactEstimator,
     severeImpact: severeImpactEstimator
   };
 };
-// console.log(covid19ImpactEstimator());
+console.log(covid19ImpactEstimator());
 
 //  module.exports = covid19ImpactEstimator;
-export default covid19ImpactEstimator;
+// export default covid19ImpactEstimator;
