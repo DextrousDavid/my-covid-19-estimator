6969b018994975dac17a03f233121c2203b9d88f
DextrousDavid <aimakhededavido@gmail.com>
2020-04-11 03:30:33 +0100

    dextrousdavid new

--- src/estimator.js
+++ src/estimator.js
@@ -14,13 +14,13 @@ const covid19ImpactEstimator = () => {
   };
   const dataCalc = () => {
     if (data.periodType === 'days') {
-      return (Math.trunc((data.timeToElapse * 1) / 30));
+      return (Math.trunc((data.timeToElapse / 30)));
     }
     if (data.periodType === 'weeks') {
-      return (Math.trunc((data.timeToElapse * 7) / 3));
+      return (Math.trunc((data.timeToElapse / 3) * 7));
     }
     if (data.periodType === 'months') {
-      return (Math.trunc((data.timeToElapse * 30) / 3));
+      return (Math.trunc((data.timeToElapse / 3) * 30));
     }
     return 0;
   };
@@ -45,5 +45,5 @@ const covid19ImpactEstimator = () => {
 };
 console.log(covid19ImpactEstimator());
 
-// module.exports = covid19ImpactEstimator;
-//  export default covid19ImpactEstimator;
+//  module.exports = covid19ImpactEstimator;
+export default covid19ImpactEstimator;
