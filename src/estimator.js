7949e816ce824600587f3391508411ec1560f39e
DextrousDavid <aimakhededavido@gmail.com>
2020-04-12 01:56:15 +0100

    without data

--- src/estimator.js
+++ src/estimator.js
@@ -1,4 +1,4 @@
-const data = {
+const info = {
   region: {
     name: 'Africa',
     avgAge: 19.7,
@@ -13,31 +13,31 @@ const data = {
 };
 const covid19ImpactEstimator = () => {
   const dataCalc = () => {
-    if (data.periodType === 'days') {
-      return (Math.trunc((data.timeToElapse / 3)));
+    if (info.periodType === 'days') {
+      return (Math.trunc((info.timeToElapse / 3)));
     }
-    if (data.periodType === 'weeks') {
-      return (Math.trunc((data.timeToElapse / 3) * 7));
+    if (info.periodType === 'weeks') {
+      return (Math.trunc((info.timeToElapse / 3) * 7));
     }
-    if (data.periodType === 'months') {
-      return (Math.trunc((data.timeToElapse / 3) * 30));
+    if (info.periodType === 'months') {
+      return (Math.trunc((info.timeToElapse / 3) * 30));
     }
     return 0;
   };
 
   const impact = {
-    currentlyInfected: data.reportedCases * 10,
-    infectionByRequestedTime: (data.reportedCases * 10) * (2 ** (dataCalc()))
+    currentlyInfected: info.reportedCases * 10,
+    infectionByRequestedTime: (info.reportedCases * 10) * (2 ** (dataCalc()))
   };
   //  SevereImpact
   const severeImpact = {
-    currentlyInfected: data.reportedCases * 50,
-    infectionByRequestedTime: (data.reportedCases * 50) * (2 ** (dataCalc()))
+    currentlyInfected: info.reportedCases * 50,
+    infectionByRequestedTime: (info.reportedCases * 50) * (2 ** (dataCalc()))
   };
   const impactEstimator = impact;
   const severeImpactEstimator = severeImpact;
   return {
-    data,
+    data: info,
     impact: impactEstimator,
     severeImpact: severeImpactEstimator
   };
