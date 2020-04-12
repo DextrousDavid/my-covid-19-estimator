f2dc2321afa67f9671f040e057a25e2949e2ba70
DextrousDavid <aimakhededavido@gmail.com>
2020-04-12 07:49:02 +0100

    used tutorial style

--- src/estimator.js
+++ src/estimator.js
@@ -1,48 +1,28 @@
-const info = {
-  region: {
-    name: 'Africa',
-    avgAge: 19.7,
-    avgDailyIncomeInUSD: 5,
-    avgDailyIncomePopulation: 0.71
-  },
-  periodType: 'days',
-  timeToElapse: 58,
-  reportedCases: 674,
-  population: 66622705,
-  totalHospitalBeds: 1380614
-};
-const covid19ImpactEstimator = () => {
+const covid19ImpactEstimator = (data) => {
+  const input = data;
   const dataCalc = () => {
-    if (info.periodType === 'days') {
-      return (Math.trunc((info.timeToElapse / 3)));
+    if (data.periodType === 'days') {
+      return (Math.trunc((data.timeToElapse / 3)));
     }
-    if (info.periodType === 'weeks') {
-      return (Math.trunc((info.timeToElapse / 3) * 7));
+    if (data.periodType === 'weeks') {
+      return (Math.trunc((data.timeToElapse / 3) * 7));
     }
-    if (info.periodType === 'months') {
-      return (Math.trunc((info.timeToElapse / 3) * 30));
+    if (data.periodType === 'months') {
+      return (Math.trunc((data.timeToElapse / 3) * 30));
     }
     return 0;
   };
-
-  const impact = {
-    currentlyInfected: info.reportedCases * 10,
-    infectionByRequestedTime: (info.reportedCases * 10) * (2 ** (dataCalc()))
-  };
-  //  SevereImpact
-  const severeImpact = {
-    currentlyInfected: info.reportedCases * 50,
-    infectionByRequestedTime: (info.reportedCases * 50) * (2 ** (dataCalc()))
-  };
-  const impactEstimator = impact;
-  const severeImpactEstimator = severeImpact;
   return {
-    data: info,
-    impact: impactEstimator,
-    severeImpact: severeImpactEstimator
+    data: input,
+    impact: {
+      currentlyInfected: data.reportedCases * 10,
+      infectionByRequestedTime: (data.reportedCases * 10) * (2 ** (dataCalc()))
+    },
+    severeImpact: {
+      currentlyInfected: data.reportedCases * 50,
+      infectionByRequestedTime: (data.reportedCases * 50) * (2 ** (dataCalc()))
+    }
   };
 };
 
-//  console.log(covid19ImpactEstimator());
-//  module.exports = covid19ImpactEstimator;
-export default covid19ImpactEstimator();
+export default covid19ImpactEstimator;
