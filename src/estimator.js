4cdd4bd304978cbf2fcd96b8a5c33f2c3878f65f
DextrousDavid <aimakhededavido@gmail.com>
2020-04-12 20:35:15 +0100

    assisstance

--- src/estimator.js
+++ src/estimator.js
@@ -1,28 +1,42 @@
-const covid19ImpactEstimator = (data) => {
-  const dataCalc = () => {
-    if (data.periodType === 'days') {
-      return (Math.trunc((data.timeToElapse / 3)));
-    }
-    if (data.periodType === 'weeks') {
-      return (Math.trunc((data.timeToElapse / 3) * 7));
-    }
-    if (data.periodType === 'months') {
-      return (Math.trunc((data.timeToElapse / 3) * 30));
-    }
-    return 0;
-  };
-
-  return {
-    impact: {
-      currentlyInfected: () => (data.reportedCases * 10),
-      infectionByRequestedTime: () => (data.reportedCases * 10) * (2 ** (dataCalc()))
-    },
-    severeImpact: {
-      reportedCases: 674,
-      currentlyInfected: () => (data.reportedCases * 50),
-      infectionByRequestedTime: () => (data.reportedCases * 50) * (2 ** (dataCalc()))
-    }
-  };
+const dataCalc = (periodType, timeToElapse) => {
+  if (periodType === 'days') {
+    return Math.trunc(timeToElapse / 3);
+  }
+  if (periodType === 'weeks') {
+    return Math.trunc((timeToElapse / 3) * 7);
+  }
+  if (periodType === 'months') {
+    return Math.trunc((timeToElapse / 3) * 30);
+  }
+  return 0;
 };
+const covid19ImpactEstimator = (data) => ({
+  // const data = {
+  //   region: {
+  //     name: 'Africa',
+  //     avgAge: 19.7,
+  //     avgDailyIncomeInUSD: 5,
+  //     avgDailyIncomePopulation: 0.71
+  //   },
+  //   periodType: 'days',
+  //   timeToElapse: 58,
+  //   reportedCases: 674,
+  //   population: 66622705,
+  //   totalHospitalBeds: 1380614
+  // };
 
+  data,
+  impact: {
+    currentlyInfected: data.reportedCases * 10,
+    infectionsByRequestedTime:
+      (data.reportedCases * 10) * (2 ** dataCalc(data.periodType, data.timeToElapse))
+  },
+  severeImpact: {
+    currentlyInfected: data.reportedCases * 50,
+    infectionsByRequestedTime:
+      (data.reportedCases * 50) * (2 ** dataCalc(data.periodType, data.timeToElapse))
+  }
+});
+
+// module.exports = covid19ImpactEstimator;
 export default covid19ImpactEstimator;
