7e3bfaf548c2a0f55012f8f8c3ec663f7b6b149e
DextrousDavid <aimakhededavido@gmail.com>
2020-04-13 13:44:11 +0100

    nnestimator

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 const dataCalc = (periodType, timeToElapse) => {
   if (periodType === 'days') {
     return Math.trunc(timeToElapse / 3);
@@ -11,21 +12,21 @@ const dataCalc = (periodType, timeToElapse) => {
   return 0;
 };
 
-//  Calculate severeCasesByRequestedTime
-const covid19ImpactEstimator = (data) => ({
-  // const data = {
-  //   region: {
-  //     name: 'Africa',
-  //     avgAge: 19.7,
-  //     avgDailyIncomeInUSD: 5,
-  //     avgDailyIncomePopulation: 0.71
-  //   },
-  //   periodType: 'days',
-  //   timeToElapse: 58,
-  //   reportedCases: 674,
-  //   population: 66622705,
-  //   totalHospitalBeds: 1380614
-  // };
+const data = {
+  region: {
+    name: 'Africa',
+    avgAge: 19.7,
+    avgDailyIncomeInUSD: 5,
+    avgDailyIncomePopulation: 0.71
+  },
+  periodType: 'days',
+  timeToElapse: 58,
+  reportedCases: 674,
+  population: 66622705,
+  totalHospitalBeds: 1380614
+};
+const covid19ImpactEstimator = () => ({
+
 
   data,
   impact: {
@@ -33,22 +34,23 @@ const covid19ImpactEstimator = (data) => ({
     infectionsByRequestedTime:
       (data.reportedCases * 10) * (2 ** dataCalc(data.periodType, data.timeToElapse)),
     severeCasesByRequestedTime:
-    (((data.reportedCases * 10) * (2 ** dataCalc(data.periodType, data.timeToElapse))) * 0.15),
+  Math.trunc(data.reportedCases * 10) * (2 ** dataCalc(data.periodType, data.timeToElapse) * 0.15),
     hospitalBedsByRequestedTime:
-    ((data.reportedCases * 10)
-    * (2 ** dataCalc(data.periodType, data.timeToElapse)) * (0.35 / data.totalHospitalBeds))
+    Math.trunc((data.reportedCases * 10)
+    * (2 ** dataCalc(data.periodType, data.timeToElapse)) * (0.35 * data.totalHospitalBeds))
   },
   severeImpact: {
     currentlyInfected: data.reportedCases * 50,
     infectionsByRequestedTime:
       (data.reportedCases * 50) * (2 ** dataCalc(data.periodType, data.timeToElapse)),
     severeCasesByRequestedTime:
-    (((data.reportedCases * 50) * (2 ** dataCalc(data.periodType, data.timeToElapse))) * 0.15),
+    Math.trunc(((data.reportedCases * 50) * (2 ** dataCalc(data.periodType, data.timeToElapse))) * 0.15),
     hospitalBedsByRequestedTime:
-    ((data.reportedCases * 50)
-    * (2 ** dataCalc(data.periodType, data.timeToElapse)) * (0.35 / data.totalHospitalBeds))
+    Math.trunc((data.reportedCases * 50)
+    * (2 ** dataCalc(data.periodType, data.timeToElapse)) * (0.35 * data.totalHospitalBeds))
   }
 });
 
+console.log(covid19ImpactEstimator());
 // module.exports = covid19ImpactEstimator;
-export default covid19ImpactEstimator;
+// export default covid19ImpactEstimator;
